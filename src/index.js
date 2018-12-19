88210678bcc5e89d189772049fe12f85e5f934bb
amodinho <u14284783@tuks.co.za>
2018-12-19 16:34:08 +0200

    added amplify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Amplify from "aws-amplify";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import config from "./config";
