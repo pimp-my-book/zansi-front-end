318841d63702bf43c0df6f55a4380f9d35729d7d
amodinho <u14284783@tuks.co.za>
2018-12-20 16:25:34 +0200

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+
 import './App.css';
 
 class App extends Component {
+  
+
   render() {
+
+    const Title = styled.h1`
+   text-align:center;
+   color: pink;
+  `;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Title>Hello! Welcome to Zansi!</Title>
         </header>
       </div>
     );
