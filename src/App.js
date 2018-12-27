ecc9e0aa69a9e19c2ce524c56d7a3d35764b1167
amodinho <u14284783@tuks.co.za>
2018-12-27 11:07:10 +0200

    added react-bootstrap and got it to work with styled components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Button } from 'react-bootstrap';
+import "./App.css";
 
 class App extends Component {
   
 
-  render() {
+	render() {
 
-    const Title = styled.h1`
+		const Title = styled.h1`
    text-align:center;
    color: pink;
   `;
 
+  const NewButton = styled(Button)`
+   padding: 20px;
+   color: pink;
+   background-color: red;
+   border: none;
+
+   :hover {
+    background-color: pink;
+   }
+  `;
 
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Title>Hello! Welcome to Zansi! <span role="img" aria-label="book emoji">📚</span></Title>
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<Title>Hello! Welcome to Zansi! <span role="img" aria-label="book emoji">📚</span></Title>
+				  <Button>Testing Bootstrap</Button>
+          <NewButton>Testing styled components with Bootstrap</NewButton>
         </header>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
