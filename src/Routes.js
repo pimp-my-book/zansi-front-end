923d7eb0b41be6091d0f3591b3f5f64b07db7866
amodinho <u14284783@tuks.co.za>
2019-01-03 14:48:05 +0200

    added unstyled Login Form

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,15 @@ import {Route,BrowserRouter as Router,Switch} from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Playground from "./containers/Playground";
+import Login from "./containers/Login";
 
 export default () => 
 <div>
 	<Switch>
 		<Route exact path="/" component={Home}/>
         <Route  path="/playground-test" component={Playground}/>
+		<Route  path="/login" component={Login}/>
+
 		<Route component={NotFound}/>
 	</Switch>
 </div>;
---
+++ src/containers/Login.js
@@ -0,0 +1,63 @@
+import React, {Component} from "react";
+import { Button, Form} from "react-bootstrap";
+
+
+export default class Login extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
+    validateForm(){
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <Form.Group controlId="email" bsSize="large">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                />
+                </Form.Group>
+                <Form.Group controlId="password" bsSize="large">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                />
+                </Form.Group>
+                <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+                </form>
+            </div>
+        );
+    }
+}
