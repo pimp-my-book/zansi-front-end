68c9234d97248fb4ce59125529969c0dec167550
amodinho <u14284783@tuks.co.za>
2019-01-03 14:27:40 +0200

    configuring components

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Route,BrowserRouter as Router,Switch} from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Playground from "./containers/Playground";
 
 export default () => 
 <div>
 	<Switch>
 		<Route exact path="/" component={Home}/>
+        <Route  path="/playground-test" component={Playground}/>
 		<Route component={NotFound}/>
 	</Switch>
 </div>;
