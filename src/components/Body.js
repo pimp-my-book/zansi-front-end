7b6d147a431b5b928151db046f8bea2113670201
amodinho <u14284783@tuks.co.za>
2019-01-06 18:07:12 +0200

    trying to fix spacing

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,36 +1,109 @@
-import React from 'react';
+import React from "react";
 import {Container,Row, Col, Image} from "react-bootstrap";
 import DisplayMedium from "./typography/DisplayMedium";
+import DisplaySmall from "./typography/DisplaySmall";
 
 const Body = () => {
 
-    const ImgURL= "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_accept_terms_4in8.svg"
-    return(
-        <Container>
+	const ImgURL= "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_accept_terms_4in8.svg";
+    const PlaceOrder = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_choose_80qg.svg";
+    const Confirmed = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_order_confirmed_1m3v.svg";
+    const Comms = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_mail_2_tqip.svg";
+    const Deliver = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_logistics_x4dc.svg";
 
-            <Row>
-              <DisplayMedium>The Process In A Nuttshell</DisplayMedium>
-                <Row>
-                    <Col></Col>
-                    <Col>
-                    {/*
-                    <Image
-                    src={ImgURL}
-                    width={200}
-                    /> */}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col></Col>
-                    <Col></Col>
-                </Row>
-                <Row>
-                    <Col></Col>
-                    <Col></Col>
-                </Row>
+
+    return(
+		<Container>
+            <Row >
+                 <DisplayMedium className="mx-auto">
+                    The Process In A Nuttshell
+                    </DisplayMedium>
             </Row>
-        </Container>
-    )
-}
+   
+			<Row >
+
+					<Col lg="4" sm="2">
+						<DisplaySmall>Sign up!
+						</DisplaySmall> 
+					</Col>
+					<Col className="justify-content-sm-center">
+                    
+						<Image
+							src={ImgURL}
+							width={200}
+						/> 
+					</Col>
+				
+				
+				
+                    </Row>
+                   
+                    <Row >
+
+					<Col>
+                    <Image
+							src={PlaceOrder}
+							width={200}
+						/> 
+						
+					</Col>
+					<Col>
+                    <DisplaySmall>Place Your Order
+						</DisplaySmall> 
+						
+					</Col>
+				
+				
+				
+                    </Row>
+
+
+
+                    <Row>
+                        <Col>
+                        <DisplaySmall>Get Confirmation
+						</DisplaySmall> 
+                        
+                        </Col>
+                        <Col>
+                        <Image
+							src={Confirmed}
+							width={200}
+						/> 
+                        </Col>
+                    </Row>
+
+                      <Row>
+                        <Col>
+                        <Image
+							src={Comms}
+							width={200}
+						/> 
+                        
+                        </Col>
+                        <Col>
+                        
+
+                        <DisplaySmall>Constant Updates
+						</DisplaySmall> 
+                        </Col>
+                    </Row>
+
+                       <Row>
+                        <Col>
+                        <DisplaySmall>Textbooks Delivered
+						</DisplaySmall> 
+                        
+                        </Col>
+                        <Col>
+                        <Image
+							src={Deliver}
+							width={200}
+						/> 
+                        </Col>
+                    </Row>
+		</Container>
+	);
+};
 
 export default Body;
