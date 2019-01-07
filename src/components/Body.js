fa81b6051a1b369c9442b8aecb5dfc957a81d4ac
amodinho <u14284783@tuks.co.za>
2019-01-07 10:44:19 +0200

    Grids are complete

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Container,Row, Col, Image} from "react-bootstrap";
 import DisplayMedium from "./typography/DisplayMedium";
 import DisplaySmall from "./typography/DisplaySmall";
+import styled from "styled-components";
 
 const Body = () => {
 
@@ -11,15 +12,98 @@ const Body = () => {
     const Comms = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_mail_2_tqip.svg";
     const Deliver = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_logistics_x4dc.svg";
 
+    const Grid =  styled.div`
+     display: grid;
+     grid-gap: 20px;
+     grid-template-columns: repeat(2,1fr);
+     margin-top: 100px;
+     margin-left:250px;
+    `
 
     return(
+        <div>
 		<Container>
-            <Row >
+            <Row>
                  <DisplayMedium className="mx-auto">
                     The Process In A Nuttshell
                     </DisplayMedium>
             </Row>
    
+		</Container>
+        		<Grid>
+                    <div>
+                    <DisplaySmall>Sign up!
+						</DisplaySmall> 
+                    </div>
+                    <div>
+                    <Image
+							src={ImgURL}
+							width={200}
+						/> 
+                    </div>
+                </Grid>
+                <Grid>
+                <div>
+                <DisplaySmall>Place Your Order
+						</DisplaySmall> 
+                    </div>
+                    <div>
+                    <Image
+							src={PlaceOrder}
+							width={200}
+						/> 
+                    </div>
+                </Grid>
+
+                <Grid>
+                <div>
+                <DisplaySmall>Get Confirmation
+						</DisplaySmall> 
+                    </div>
+                    <div>
+                    <Image
+							src={Confirmed}
+							width={200}
+						/> 
+                    </div>
+                    
+                    </Grid>
+                    <Grid>
+                    <div>
+                <DisplaySmall>Constant Updates
+						</DisplaySmall> 
+                    </div>
+                    <div>
+                    <Image
+							src={Comms}
+							width={200}
+						/> 
+                    </div>
+
+</Grid>
+                    <Grid>
+                    <div>
+                <DisplaySmall>Textbooks Delivered
+						</DisplaySmall> 
+                    </div>
+                    <div>
+                    <Image
+							src={Deliver}
+							width={200}
+						/> 
+                    </div>
+                    
+                    </Grid>
+
+                </div>
+
+	);
+};
+
+export default Body;
+
+/*
+
 			<Row >
 
 					<Col lg="4" sm="2">
@@ -102,8 +186,5 @@ const Body = () => {
 						/> 
                         </Col>
                     </Row>
-		</Container>
-	);
-};
 
-export default Body;
+*/
