da8edbc51a28fbcc8520ea526a869ca1f37f939e
amodinho <u14284783@tuks.co.za>
2019-01-07 13:03:25 +0200

    netlify change

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ const client = new ApolloClient({
 const GlobalStyle = createGlobalStyle`
 
 
-:root {
+
 
 	@import url('https://fonts.googleapis.com/css?family=Poppins|Paytone+One');
 	--rose-pink: #ff6bd6;
@@ -130,7 +130,7 @@ const GlobalStyle = createGlobalStyle`
 	
 	 
 	
-}
+
 
 html {
 	position: relative;
