e6c56f5ae54ca136c0324ee7d1a53fc6d990f357
amodinho <u14284783@tuks.co.za>
2019-01-09 12:28:35 +0200

    done with 2

--- src/containers/pages/HowItWorks.js
+++ src/containers/pages/HowItWorks.js
@@ -8,7 +8,7 @@ import Textbody from "../../components/typography/Textbody";
 
 const HowItWorks = () => {
 
-  const Header = styled.header`
+	const Header = styled.header`
    display: grid;
    grid-gap: 2px;
    grid-template-columns: repeat(2, 1fr);
@@ -22,36 +22,55 @@ const HowItWorks = () => {
    }
   `;
 
-  const Progress = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_in_progress_ql66.svg";
+	const Progress = "https://s3.amazonaws.com/zansi-static-assest/Illustrations/undraw_in_progress_ql66.svg";
 
 
+  const TextDiv = styled.div`
+    margin-left: 40px;
+  `;
 
-    return (
-        <div>
-           <Header>
-             <div className="ml-4">
-             <DisplayMedium>How It works</DisplayMedium>
-             <Heading className=" mt-4">This page is meant to fimilarize you with the process of how the we at Pimp My Book will get you the books you ordered.</Heading>
 
-             </div>
-               <Image
-               src={Progress}
-               width={200}
-               fluid
-               className=" d-none d-lg-block"
-               />
+	return (
+		<div>
+			<Header>
+				<div className="ml-4">
+					<DisplayMedium>How It works</DisplayMedium>
+					<Heading className=" mt-4">This page is meant to fimilarize you with the process of how the we at Pimp My Book will get you the books you ordered.</Heading>
 
-           </Header>
+				</div>
+				<Image
+					src={Progress}
+					width={200}
+					fluid
+					className=" d-none d-lg-block"
+				/>
 
-           <div>
-              <Subheading> 1. Placing Orders</Subheading>
-              <Textbody>Go to www.zansi.co.za 
-Click “order now” and complete the order google form
-</Textbody>
-           </div>
+			</Header>
+
+			<TextDiv>
+				<Subheading> 1. Placing Orders</Subheading>
+				<Textbody>Go to www.zansi.co.za 
+           Click “order now” and complete the order google form
+				</Textbody>
+			</TextDiv>
            
-        </div>
-    )
-}
+      <TextDiv>
+         <Subheading>2.	Status Updates</Subheading>
+         <Textbody>You will get the following order status updates</Textbody>
+         <ul>
+           <li>Confirmation of order received – 24 hours</li>
+           <li>ETA (Expected Turn Around) </li>
+           <li>Dispatched  - Once the book is on its way to you</li>
+           <li>Ready for collection -  When the book is ready to be collected at one of our stores or pick up points</li>
+           <li>Delivered/ Collected – When the courier has delivered the book to you.</li>
+           <li>On Order (ETA)</li>
+         </ul>
+
+         <Textbody>We will use the email address that you provide to update you on your order status. </Textbody>
+         <Textbody>You are also welcome to contact us at: moshal@pimpmybook.co.za or whatsapp 065 850 5225  (always provide your student number to help us to quickly identify your order).</Textbody>
+        </TextDiv>
+		</div>
+	);
+};
 
 export default HowItWorks;
