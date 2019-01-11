f6b58132f5a7d32837131f5093e7e154952d80c3
amodinho <u14284783@tuks.co.za>
2019-01-11 10:36:30 +0200

    created order test

---
+++ src/__tests__/Order.test.js
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////
+/* Component behaviours
+
+ - User should call the place order mututaion
+ : Enter order details => 
+ : Click Submit button => 
+ : Validate inputs =>
+ : Open modal for success =>
+ : Redirect to place order page 
+ 
+ - Post Conditon for success: 
+ There should be a modal indicating the orderId and name of order
+Then be redirected to place order page:
+
+
+ -Post Condition for failure:
+ There should be an alert saying that there was a failure 
+ in placing the order
+
+
+
+
+
+
+*/
+/////////////////////////////////////////////////////
