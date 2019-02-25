3591e33ded020406632e1023f0ca6f2290eaaf14
amodinho <u14284783@tuks.co.za>
2019-02-25 11:52:41 +0200

    Added a snapshot for the dashboard test

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import Dashboard from '../containers/Dashboard';
-import {mount,configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import {wrap} from 'module';
+import {MockedProvider} from "react-apollo/test-utils";
+import {ORDER_LIST}from "../graphql/Queries";
+import Dashboard from "../containers/Dashboard";
+import {mount,configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import LoadingSpinner from "../components/LoadingSpinner";
+import {wrap} from "module";
 configure({adapter: new Adapter()});
 /////////////////////////////////////////////////
 /* Comoponent Behaviours
@@ -34,15 +37,73 @@ Component Contracts
 
 */
 //////////////////////////////////////////////////
-describe('<Dashboard/>', ()=> {
+
+
+const mocks = [
+	{
+		request: {
+			mutation: ORDER_LIST
+		},
+		result: {
+			data: {
+				order1: {
+					orderId: "34444444343",
+					name: "SBTRK",
+					status: "Recieved",
+                    dateOrdered: "2 days ago",
+                    title: "Such a Savage"
+				}, 
+        
+				order2: {
+					orderId: "4535343423423",
+					name: "Bakar",
+					status: "Recieved",
+                    dateOrdered: "2 days ago",
+                    title: "One Way Road"
+				}, 
+				order3: {
+					orderId: "235234324",
+					name: "Brooke",
+					status: "Cancel Requested",
+                    dateOrdered: "2 days ago",
+                    title: "Say My Name Across the Boarder"
+				}, 
+			}
+		}
+	}
+];
+
+
+describe("<Dashboard/>", ()=> {
 
   
-    it('renders without crashing', () => {
-      renderer.create(
-          <Dashboard/>
-      )
-    });
+	it("renders without crashing", () => {
+		renderer.create(
+            <MockedProvider mocks={[]}>
+               <Dashboard/>
+            </MockedProvider>
+			
+		);
+	});
+
+	test("Get Snapshot", () => {
+		const wrapper = shallow(<Dashboard/>);
+		expect(wrapper).toMatchSnapshot();
+	});
     
+
+    it("renders the loading state", () => {
+        
+           const wrapper = mount(
+                <MockedProvider mocks={[]}>
+                   <Dashboard/>
+                </MockedProvider>
+                
+            );
+
+            expect(wrapper).toContain(<LoadingSpinner/>);
+        
+    })
     
-    });
+});
     
